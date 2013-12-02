d552a9e7fdb812b193ea9b2df9c6818a486aa5a6
Gary O' Donoghue <gary.odonoghue@gmail.com>
2013-12-02 21:24:17 +0000

    Add record sound and second page

---
+++ sample1/Record.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+using System.Windows.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace sample1
+{
+    public partial class Record : PhoneApplicationPage
+    {
+        Microphone microphone = Microphone.Default;
+        byte[] buffer;
+        MemoryStream stream = new MemoryStream();
+        SoundEffect sound;
+
+        public Record()
+        {
+            InitializeComponent();
+
+            // Timer to simulate the XNA Game Studio game loop (Microphone is from XNA Game Studio)
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(33);
+            dt.Tick += delegate { try { FrameworkDispatcher.Update(); } catch { } };
+            dt.Start();
+            microphone.BufferReady += new EventHandler<EventArgs>(microphone_BufferReady);
+        }
+
+        void microphone_BufferReady(object sender, EventArgs e)
+        {
+            microphone.GetData(buffer);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private void recordButton_Click(object sender, RoutedEventArgs e)
+        {
+            microphone.BufferDuration = TimeSpan.FromMilliseconds(1000);
+            buffer = new byte[microphone.GetSampleSizeInBytes(microphone.BufferDuration)];
+            microphone.Start();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (microphone.State == MicrophoneState.Started)
+            {
+                microphone.Stop();
+            }
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            sound = new SoundEffect(stream.ToArray(), microphone.SampleRate, AudioChannels.Mono);
+            sound.Play();
+        }
+    }
+}

ab09f745dec256682ed70edecbe975cfd3c64597
Colm Seale <colm369@hotmail.com>
2013-12-09 00:32:30 +0000

    created a main menu for the application. Animations on button press would be nice

---
+++ sample1/MainMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace sample1
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToChords(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Chords.xaml", UriKind.Relative));
+        }
+
+        private void GoToTuner(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Tuning.xaml", UriKind.Relative));
+        }
+
+        private void GoToMetronome(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Metronome.xaml", UriKind.Relative));
+        }
+
+        private void GoToLooper(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Record.xaml", UriKind.Relative));
+        }
+
+    }
+}
